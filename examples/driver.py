8e7009b6485e56545aec3a3161bfe26503ab1d1d
Stephen Liu <stephen.liu@ucalgary.ca>
2023-09-06 18:41:26 -0600

    driver test

---
+++ examples/driver.py
@@ -0,0 +1,20 @@
+import numpy as np
+from my_python_package.operators import (
+        add,
+        multiply,
+        )
+def main():
+
+    # test for scalars
+    print(f'add(1, 3): {add(1, 3)}')
+    print(f'multiply(2, 12.): {multiply(2, 12.)}')
+    
+    # test for arrays
+    A = np.array([[1, 2, 3], [4, 5, 6]])
+    B = 2. * np.ones(A.shape)
+    print(f'A:\n{A}')
+    print(f'B:\n{B}')
+    print(f'add(A, B):\n{add(A, B)}')
+    print(f'multiply(A, B):\n{multiply(A, B)}')
+if __name__ == '__main__':
+ main()
